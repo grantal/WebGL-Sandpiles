d1e3be03c6d978a239c6013ec91322267be0358c
grantal <AlexV348@gmail.com>
2017-11-02 14:03:27 -0700

    made a 3d box

--- js/shader_interface.js
+++ js/shader_interface.js
@@ -66,6 +66,7 @@ SAND.prototype.step = function() {
 
 SAND.prototype.draw = function() {
     var gl = this.gl;
+    /*
     gl.bindFramebuffer(gl.FRAMEBUFFER, null);
     gl.bindTexture(gl.TEXTURE_2D, this.textures.front);
 
@@ -89,18 +90,29 @@ SAND.prototype.draw = function() {
 		.uniform('color', this.color)
 		.draw(gl.TRIANGLE_STRIP, 4);	
     return this;
+    */
+    //gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+    this.xrot = this.xrot + 0.5;
+    this.yrot = this.yrot + 0.1;
+    drawScene(gl, this.programInfo, this.buffers3d, this.xrot, this.yrot);
+    return this;
 };
 
 SAND.prototype.get = function() {
+    
     var gl = this.gl, w = this.statesize.x, h = this.statesize.y;
+    /*
     gl.bindFramebuffer(gl.FRAMEBUFFER, this.framebuffers.step);
     gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0,
                             gl.TEXTURE_2D, this.textures.front, 0);
+                            */
     var state = new Float32Array(w * h * 4);
+    /*
     gl.readPixels(0, 0, w, h, gl.RGBA, gl.FLOAT, state);
 	for (var i = 0; i < state.length; i++) {   
 		state[i] = state[i]*max;
 	}  
+        */
     return state;
 	
 };
@@ -115,11 +127,134 @@ SAND.prototype.set = function(state) {
         rgba[i + 3] = state[i + 3]/max;
     }
 	 
+    /*
     gl.bindTexture(gl.TEXTURE_2D, this.textures.front);
+    
     gl.texSubImage2D(gl.TEXTURE_2D, 0, 0, 0,
                      this.statesize.x, this.statesize.y,
                      gl.RGBA, gl.FLOAT, rgba);
-					 
+*/					 
     return this;
 };
 
+
+//
+// Draw the scene. Stolen from mozila webgl example
+//
+function drawScene(gl, programInfo, buffers, xRotation, yRotation) {
+  
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);  // Clear to black, fully opaque
+  gl.clearDepth(1.0);                 // Clear everything
+  gl.enable(gl.DEPTH_TEST);           // Enable depth testing
+  gl.depthFunc(gl.LEQUAL);            // Near things obscure far things
+
+  // Clear the canvas before we start drawing on it.
+
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+  // Create a perspective matrix, a special matrix that is
+  // used to simulate the distortion of perspective in a camera.
+  // Our field of view is 45 degrees, with a width/height
+  // ratio that matches the display size of the canvas
+  // and we only want to see objects between 0.1 units
+  // and 100 units away from the camera.
+
+  const fieldOfView = 45 * Math.PI / 180;   // in radians
+  const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
+  const zNear = 0.1;
+  const zFar = 100.0;
+  const projectionMatrix = mat4.create();
+
+  // note: glmatrix.js always has the first argument
+  // as the destination to receive the result.
+  mat4.perspective(projectionMatrix,
+                   fieldOfView,
+                   aspect,
+                   zNear,
+                   zFar);
+
+  // Set the drawing position to the "identity" point, which is
+  // the center of the scene.
+  const modelViewMatrix = mat4.create();
+
+  // Now move the drawing position a bit to where we want to
+  // start drawing the square.
+
+  mat4.translate(modelViewMatrix,     // destination matrix
+                 modelViewMatrix,     // matrix to translate
+                 [-0.0, 0.0, -6.0]);  // amount to translate
+  mat4.rotate(modelViewMatrix,  // destination matrix
+              modelViewMatrix,  // matrix to rotate
+              yRotation,        // amount to rotate in radians
+              [1, 0, 0]);       // axis to rotate around (Y)
+  mat4.rotate(modelViewMatrix,  // destination matrix
+              modelViewMatrix,  // matrix to rotate
+              xRotation,// amount to rotate in radians
+              [0, 1, 0]);       // axis to rotate around (X)
+
+  // Tell WebGL how to pull out the positions from the position
+  // buffer into the vertexPosition attribute
+  {
+    const numComponents = 3;
+    const type = gl.FLOAT;
+    const normalize = false;
+    const stride = 0;
+    const offset = 0;
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
+    gl.vertexAttribPointer(
+        programInfo.attribLocations.vertexPosition,
+        numComponents,
+        type,
+        normalize,
+        stride,
+        offset);
+    gl.enableVertexAttribArray(
+        programInfo.attribLocations.vertexPosition);
+  }
+
+  // Tell WebGL how to pull out the colors from the color buffer
+  // into the vertexColor attribute.
+  {
+    const numComponents = 4;
+    const type = gl.FLOAT;
+    const normalize = false;
+    const stride = 0;
+    const offset = 0;
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffers.color);
+    gl.vertexAttribPointer(
+        programInfo.attribLocations.vertexColor,
+        numComponents,
+        type,
+        normalize,
+        stride,
+        offset);
+    gl.enableVertexAttribArray(
+        programInfo.attribLocations.vertexColor);
+  }
+
+  // Tell WebGL which indices to use to index the vertices
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffers.indices);
+
+  // Tell WebGL to use our program when drawing
+
+  gl.useProgram(programInfo.program);
+
+  // Set the shader uniforms
+
+  gl.uniformMatrix4fv(
+      programInfo.uniformLocations.projectionMatrix,
+      false,
+      projectionMatrix);
+  gl.uniformMatrix4fv(
+      programInfo.uniformLocations.modelViewMatrix,
+      false,
+      modelViewMatrix);
+
+  {
+    const vertexCount = 36;
+    const type = gl.UNSIGNED_SHORT;
+    const offset = 0;
+    gl.drawElements(gl.TRIANGLES, vertexCount, type, offset);
+  }
+
+}
