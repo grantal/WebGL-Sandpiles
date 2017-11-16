5b6e456c3d23a6b89eac6db023c8133486135be8
grantal <AlexV348@gmail.com>
2017-11-16 13:09:41 -0800

    zoomed in some

--- js/shader_interface.js
+++ js/shader_interface.js
@@ -179,7 +179,7 @@ function drawScene(gl, programInfo, buffers, texture, xRotation, yRotation, sand
 
   mat4.translate(modelViewMatrix,     // destination matrix
                  modelViewMatrix,     // matrix to translate
-                 [-0.0, 0.0, -6.0]);  // amount to translate
+                 [-0.0, 0.0, -4.0]);  // amount to translate
   mat4.rotate(modelViewMatrix,  // destination matrix
               modelViewMatrix,  // matrix to rotate
               yRotation,        // amount to rotate in radians
